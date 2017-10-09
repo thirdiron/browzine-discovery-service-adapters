a61d4814b7cc429e432da3d46881614c4cea5568
Tian Davis <tiandavis@gmail.com>
2017-10-09 13:50:41 -0400

    Updated expected API domain.

--- src/browzine-summon-adapter.js
+++ src/browzine-summon-adapter.js
@@ -1,6 +1,6 @@
 // Begin BrowZine-Summon Integration Code
 angular.module("summonApp.directives")
-.constant("api", "http://localhost:3000/public/v1/libraries/XXX")
+.constant("api", "https://api.thirdiron.com/public/v1/libraries/XXX")
 .constant("apiKey", "ENTER API KEY")
 .constant("bookIcon", "https://s3.amazonaws.com/thirdiron-assets/images/integrations/browzine_open_book_icon.png")
 .directive("documentSummary", ["$http", "$sce", "api", "apiKey", "bookIcon", function(http, sce, api, apiKey, bookIcon) {
