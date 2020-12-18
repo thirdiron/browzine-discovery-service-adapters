1bdb54fdbc1fa7347f47702c75fa772a2df96a6c
Tian Davis <tiandavis@gmail.com>
2020-12-17 23:15:43 -0500

    show article link by default failing tests.

--- tests/unit/models/summon.js
+++ tests/unit/models/summon.js
@@ -1117,8 +1117,8 @@ describe("Summon Model >", function() {
       delete browzine.articleLinkEnabled;
     });
 
-    it("should not show article link by default when configuration property is undefined or null", function() {
-      expect(summon.showArticleLink()).toEqual(false);
+    it("should show article link by default when configuration property is undefined or null", function() {
+      expect(summon.showArticleLink()).toEqual(true);
     });
 
     it("should show article link when configuration property is true", function() {
